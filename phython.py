7aecf62e6e075e41912db09aed2b9811e080bb0b
vadheraryan <vadheraryan649@gmail.com>
2026-01-05 11:30:00 +0530

    Add files via upload

---
+++ phython.py
@@ -0,0 +1,34 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print (aryan)
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    print (aryan)
+NameError: name 'aryan' is not defined
+x="hello word"
+print(x)
+hello word
+x=3
+print(x)
+3
+x="true"
+print(x)
+true
+x=["apple" "banana" "cherry"]
+print(x)
+['applebananacherry']
+x=["apple","banana","cherry"]
+print(x)
+['apple', 'banana', 'cherry']
+x=["name","john","age","40"]
+print(x)
+['name', 'john', 'age', '40']
+x="none"
+print(x)
+none
+x="3j"
+print(x)
+3j
+x="145.25"
+print(x)
+145.25
