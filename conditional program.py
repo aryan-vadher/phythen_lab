5493e84518e6570963ab851c274de22db66f0924
aryan vadher <vadheraryan649@gmail.com>
2026-02-11 08:50:42 +0530

    Add files via upload

---
+++ conditional program.py
@@ -0,0 +1,38 @@
+#voting eligible program:-
+"""age = int(input("enter your age:"))
+if age>=18:
+    print("you are eligible .")
+else:
+    print("you are not eligible .")"""
+    
+#licence eligible program:-
+"""age = int(input("ENTER YOUR AGE:"))
+if age>=18:
+    print("congratulations you achive licence ... ") 
+else:
+    print("sorry not now try after 18 years...") """
+    
+#positive and negative program:-
+"""num = int(input("ENTER YOUR LUCKY NUMBER:"))
+if num>0:
+    print("WOW! YOUR LUCKY NUMBER IS POSITIVE...")
+elif num<0:
+    print("OHH! YOUR LUCKY NUMBER IS NEGATIVE...")
+else:
+    print("HURRAH! YOUR LUCKY NUMBER IS MINDI...")"""                 
+        
+#odd and even program:
+'''khush = int(input("ENTER YOUR NUMBER:"))
+if (khush % 2) == 0:
+    print("NUMBER IS EVEN...") 
+else:
+    print("NUMBER IS ODD...") '''
+    
+#mark's program:-
+aryan = int(input("ENTER YOUR MARK :-"))
+if aryan>=90:
+    print("YOU GET A GRADE...")      
+elif aryan>=70:
+    print("YOU GET B GRADE...")  
+else:
+    print("YOU ONLY PASS...")           
