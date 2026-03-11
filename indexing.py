f32670439c97cc24b3385c86206e2f8c999ec19c
aryan vadher <vadheraryan649@gmail.com>
2026-03-11 08:10:50 +0530

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,20 @@
+#1 positional array indexing
+arr = [10,"10", "20", "30", "40", "50"]
+print(arr[0])
+print(arr[2])
+print(arr[4])
+
+#negative
+
+arr = [10,"10", "20", "30", "40", "50"]
+print(arr[-1])
+print(arr[-2])
+print(arr[-4])
+
+
+# modify
+
+from array import array
+my_array = array ('i',[1,2,3,4])
+my_array [0] = 5
+print(my_array)
